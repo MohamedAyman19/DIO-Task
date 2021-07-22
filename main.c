551afc2470e644ef2d0f57a664fc6d462426d60d
MohamedAyman19 <87302157+MohamedAyman19@users.noreply.github.com>
2021-07-22 21:30:30 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+ * DIO_driver.c
+ *
+ * Created: 22-Jul-21 4:05:55 PM
+ * Author : ayman
+ */ 
+
+#define F_CPU 8000000UL
+#include"Macros.h"
+#include"TypeDef.h"
+#include"DIO_Int.h"
+int main(void)
+{
+	DIO_sint8_tSetPortDirection(PortA,PORT_OUTPUT);
+	DIO_sint8_tSetPortDirection(PortC,PORT_INPUT);
+
+    /* Replace with your application code */
+    while (1) 
+    {
+		if(DIO_sint8_t_tGetPinValue(PortC,PIN0)==PIN_HIGH)
+		{
+			//for(uint8_t i=0;i<90;i++); ///                           for debouncing 
+			if(DIO_sint8_t_tGetPinValue(PortC,0)==PIN_HIGH)
+			{
+				DIO_sint8_tTogglePin(PortA,PIN0);
+				while(DIO_sint8_t_tGetPinValue(PortC,PIN0)==1);
+			}
+
+		}
+		
+			
+    }
+}
+
